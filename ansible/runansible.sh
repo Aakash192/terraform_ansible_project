ad18193cdd1e19936cc6c63e073bf8702112db24
Aakash192 <145707623+Aakash192@users.noreply.github.com>
2024-04-18 16:32:02 -0400

    Add files via upload

---
+++ ansible/runansible.sh
@@ -0,0 +1,10 @@
+cd /home/aakash/automation/automation_assignment2-main/ansible
+echo "------------------------------------"
+echo "Running ansible playbook"
+echo "------------------------------------"
+
+ansible-playbook --verbose n01603990-playbook.yml
+
+echo "------------------------------------"
+echo "Completed ansible playbook"
+echo "------------------------------------"
